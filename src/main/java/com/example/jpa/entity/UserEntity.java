8173b32510408ff8f76c9e56d1de4a4ec48f871e
myeonghyejin <myeonghyejin@icloud.com>
2023-10-16 14:43:45 +0900

    Basic JPA

---
+++ src/main/java/com/example/jpa/entity/UserEntity.java
@@ -0,0 +1,50 @@
+package com.example.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id // primary Key
+    private String email;
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public UserEntity(String email, String name, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.email = email;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UserEntity() {
+
+    }
+}
